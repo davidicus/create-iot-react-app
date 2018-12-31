1626a56d243e33509bc8b1bf64bbd1ec1d0940ab
davidicus <david@david-conner.com>
2018-12-31 15:37:27 -0600

    :green_heart: fixing bad test command

--- index.js
+++ index.js
@@ -1,89 +1,81 @@
 #!/usr/bin/env node
 
-const shell = require("shelljs");
-const colors = require("colors");
-const fs = require("fs");
-const templates = require("./templates/templates.js");
+const shell = require('shelljs');
+const colors = require("colors"); // eslint-disable-line
+const fs = require('fs');
+const templates = require('./templates/templates.js');
 
 const appName = process.argv[2];
 const appDirectory = `${process.cwd()}/${appName}`;
 
-const createReactApp = () => {
-  return new Promise(resolve => {
-    if (appName) {
-      shell.exec(`npx create-react-app ${appName}`, () => {
-        console.log("Created react app");
-        resolve(true);
-      });
-    } else {
-      console.log("\nNo app name was provided.".red);
-      console.log("\nProvide an app name in the following format: ");
-      console.log("\ncreate-iot-react-app ", "app-name\n".cyan);
-      resolve(false);
-    }
-  });
-};
-
-const cdIntoNewApp = () => {
-  return new Promise(resolve => {
-    shell.exec(`cd ${appName}`, () => {
-      resolve();
+const createReactApp = () => new Promise((resolve) => {
+  if (appName) {
+    shell.exec(`npx create-react-app ${appName}`, () => {
+      console.log('Created react app');
+      resolve(true);
     });
-  });
-};
+  } else {
+    console.log('\nNo app name was provided.'.red);
+    console.log('\nProvide an app name in the following format: ');
+    console.log('\ncreate-iot-react-app ', 'app-name\n'.cyan);
+    resolve(false);
+  }
+});
 
-const installPackages = () => {
-  return new Promise(resolve => {
-    console.log(
-      "\nInstalling storybook, node-sass,  @reach/router, react-intl, carbon-components, carbon-components-react, carbon-icons, classnames\n"
-        .cyan
-    );
-    shell.exec(
-      `npx storybook && yarn add node-sass @reach/router react-intl carbon-components carbon-components-react carbon-icons classnames`,
-      () => {
-        console.log("\nFinished installing packages\n".green);
-        resolve();
-      }
-    );
+const cdIntoNewApp = () => new Promise((resolve) => {
+  shell.exec(`cd ${appName}`, () => {
+    resolve();
   });
-};
+});
 
-const updateTemplates = () => {
-  return new Promise(resolve => {
-    let promises = [];
-    Object.keys(templates).forEach((fileName, i) => {
-      promises[i] = new Promise(res => {
-        fs.writeFile(
-          `${appDirectory}/src/${fileName}`,
-          templates[fileName],
-          function(err) {
-            if (err) {
-              return console.log(err);
-            }
-            res();
+const installPackages = () => new Promise((resolve) => {
+  console.log(
+    '\nInstalling storybook, node-sass,  @reach/router, react-intl, carbon-components, carbon-components-react, carbon-icons, classnames\n'
+      .cyan,
+  );
+  shell.exec(
+    'npx storybook && yarn add node-sass @reach/router react-intl carbon-components carbon-components-react carbon-icons classnames',
+    () => {
+      console.log('\nFinished installing packages\n'.green);
+      resolve();
+    },
+  );
+});
+
+const updateTemplates = () => new Promise((resolve) => {
+  const promises = [];
+  Object.keys(templates).forEach((fileName, i) => {
+    promises[i] = new Promise((res) => {
+      fs.writeFile(
+        `${appDirectory}/src/${fileName}`,
+        templates[fileName],
+        (err) => {
+          if (err) {
+            return console.log(err);
           }
-        );
-      });
-    });
-    Promise.all(promises).then(() => {
-      resolve();
+          res();
+        },
+      );
     });
   });
-};
+  Promise.all(promises).then(() => {
+    resolve();
+  });
+});
 
 const run = async () => {
   const success = await createReactApp();
   if (!success) {
     console.log(
-      "Something went wrong while trying to create a new React app using create-react-app"
-        .red
+      'Something went wrong while trying to create a new React app using create-react-app'
+        .red,
     );
     return false;
   }
   await cdIntoNewApp();
   await installPackages();
   await updateTemplates();
-  console.log("All done");
+  console.log('All done');
 };
 
 run();
