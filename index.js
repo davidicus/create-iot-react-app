c52620cfd335ad19c9538a3bd2d4e36e2dd6e765
davidicus <david@david-conner.com>
2019-07-23 22:11:34 -0400

    feat(carbon): update template to include appropriate carbon packages and add UI shell

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const { exec } = require("child_process");
-const templates = require("./templates/templates.js");
+const fs = require('fs');
+const { exec } = require('child_process');
+const templates = require('./templates/templates.js');
 
 const appName = process.argv[2];
 const appDirectory = `${process.cwd()}/${appName}`;
 
-const createReactApp = () =>
-  new Promise(resolve => {
+const createReactApp = () => new Promise((resolve) => {
     if (appName) {
       exec(`npx create-react-app ${appName}`, (error, stdout, stderr) => {
         if (error) {
@@ -17,58 +16,59 @@ const createReactApp = () =>
         }
         console.log(`stdout: ${stdout}`);
         console.log(`stderr: ${stderr}`);
-        console.log("Created react app");
+        console.log('Created react app, now installing storybook.');
         resolve(true);
       });
     } else {
-      console.log("\nNo app name was provided.");
-      console.log("\nProvide an app name in the following format: ");
-      console.log("\ncreate-iot-react-app ", "app-name\n");
+      console.log('\nNo app name was provided.');
+      console.log('\nProvide an app name in the following format: ');
+      console.log('\ncreate-iot-react-app ', 'app-name\n');
       resolve(false);
     }
   });
 
-const cdAndStorybook = () =>
-  new Promise((resolve, reject) => {
-    exec(`cd ${appName} && npx storybook`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        reject();
-      }
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-      resolve();
-    });
+const cdAndStorybook = () => new Promise((resolve, reject) => {
+    exec(
+      `cd ${appName} && npx -p @storybook/cli sb init`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          reject();
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        console.log('Storybook added, now installing supporting packages.');
+        resolve();
+      },
+    );
   });
 
-const installPackages = () =>
-  new Promise(resolve => {
+const installPackages = () => new Promise((resolve) => {
     console.log(
-      "\nInstalling node-sass, @reach/router, react-intl, carbon-components, carbon-components-react, carbon-icons, classnames\n"
+      '\nInstalling node-sass, @reach/router, react-intl, carbon-components, carbon-components-react, carbon-icons, @carbon/icons,  @carbon/icons-react, @carbon/colors, @carbon/grid, @carbon/layout, @carbon/themes, carbon-addons-iot-react@beta, wiot-theme, classnames\n',
     );
     exec(
-      `yarn --cwd ${appDirectory} add node-sass @reach/router react-intl carbon-components carbon-components-react carbon-icons classnames @babel/core babel-loader@8.0.4`,
+      `yarn --cwd ${appDirectory} add node-sass @reach/router react-intl carbon-components carbon-components-react carbon-icons @carbon/icons @carbon/icons-react @carbon/colors @carbon/grid @carbon/layout @carbon/themes carbon-addons-iot-react@beta classnames`,
       () => {
-        console.log("\nFinished installing packages\n");
+        console.log('\nFinished installing packages\n');
         resolve();
-      }
+      },
     );
   });
 
-const updateTemplates = () =>
-  new Promise(resolve => {
+const updateTemplates = () => new Promise((resolve) => {
     const promises = [];
     Object.keys(templates).forEach((fileName, i) => {
-      promises[i] = new Promise(res => {
+      promises[i] = new Promise((res) => {
         fs.writeFile(
           `${appDirectory}/src/${fileName}`,
           templates[fileName],
-          err => {
+          (err) => {
             if (err) {
               return console.log(err);
             }
             res();
-          }
+          },
         );
       });
     });
@@ -78,17 +78,18 @@ const updateTemplates = () =>
   });
 
 const run = async () => {
+  console.log('Starting install of WIoT React app, this may take a minute.');
   const success = await createReactApp();
   if (!success) {
     console.log(
-      "Something went wrong while trying to create a new React app using create-react-app"
+      'Something went wrong while trying to create a new React app using create-react-app',
     );
     return false;
   }
   await cdAndStorybook();
   await installPackages();
   await updateTemplates();
-  console.log("All done");
+  console.log('All done');
 };
 
 run();
--- templates/App.js
+++ templates/App.js
@@ -1,41 +1,128 @@
 module.exports = `
-/**
- * IBM Confidential
- * OCO Source Materials
- * 5900-A0N
- * © Copyright IBM Corp. 2018
- * The source code for this program is not published or otherwise divested of its
- * trade secrets, irrespective of what has been deposited with the U.S. Copyright
- * Office.
- */
 
-import React, { Component } from 'react';
-import logo from './logo.svg'
-import './App.scss'
+import React, { Component } from "react";
+import AppSwitcher from "@carbon/icons-react/lib/app-switcher/24";
+import Chip from "@carbon/icons-react/lib/chip/24";
+import Group from "@carbon/icons-react/lib/group/24";
+import AddFilled16 from "@carbon/icons-react/lib/add--filled/16";
+import { HeaderContainer } from "carbon-components-react/lib/components/UIShell";
+import {
+  Header,
+  SideNav,
+  ButtonEnhanced,
+  PageHero
+} from "carbon-addons-iot-react";
 
+import "./App.scss";
+
+const RouterComponent = ({ children, ...rest }) => (
+  <div {...rest}>{children}</div>
+);
+
+const links = [
+  {
+    icon: AppSwitcher,
+    isEnabled: true,
+    metaData: {
+      onClick: () => {},
+      tabIndex: 0,
+      label: "Boards",
+      element: RouterComponent
+    },
+    linkContent: "Boards"
+  },
+  {
+    current: true,
+    isEnabled: true,
+    icon: Chip,
+    metaData: {
+      label: "Devices",
+      href: "https://google.com",
+      element: "a",
+      target: "_blank"
+    },
+    linkContent: "Devices"
+  },
+  {
+    isEnabled: true,
+    icon: Group,
+    metaData: {
+      label: "Members",
+      element: "button"
+    },
+    linkContent: "Members",
+    childContent: [
+      {
+        metaData: {
+          label: "Devices",
+          onClick: () => {},
+          element: "button"
+        },
+        content: "Yet another link"
+      }
+    ]
+  }
+];
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main className="app">
+        <HeaderContainer
+          render={({ isSideNavExpanded, onClickSideNavExpand }) => (
+            <>
+              <Header
+                actionItems={[
+                  {
+                    label: "user",
+                    onClick: () => {},
+                    btnContent: (
+                      <p className="header-link">
+                        JohnDoe@ibm.com<span>TenantId: Acme</span>
+                      </p>
+                    )
+                  }
+                ]}
+                appName="Watson IoT"
+                isSideNavExpanded={isSideNavExpanded}
+                onClickSideNavExpand={onClickSideNavExpand}
+              />
+              <SideNav
+                links={links}
+                isSideNavExpanded={isSideNavExpanded}
+                onClickSideNavExpand={onClickSideNavExpand}
+              />
+            </>
+          )}
+        />
+
+        <section className="main">
+          <PageHero
+            className="main-page-hero"
+            title="Watson IoT Starter App"
+            blurb="You can add some descriptive text that is associated with your page by adding the blurb prop to the PageHero element."
+          />
+          <div className="main-content">
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <ButtonEnhanced
+              href="https://beta.carbon-addons-iot-react.com/"
+              target="_blank"
+              iconDescription="Button icon"
+              onClick={() => {}}
+              onFocus={() => {}}
+              renderIcon={AddFilled16}
+            >
+              Get Components
+            </ButtonEnhanced>
+          </div>
+        </section>
+      </main>
     );
   }
 }
 
 export default App;
+
 
 `;
