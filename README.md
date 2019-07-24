14ef177257cdb643716446a8a04cacc84e59013f
davidicus <david@david-conner.com>
2019-07-23 22:56:05 -0400

    feat(readme): update readme and add styles to console outputs

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # create-iot-react-app [![Build Status](https://travis-ci.org/davidicus/create-iot-react-app.svg?branch=master)](https://travis-ci.org/davidicus/create-iot-react-app)
 
-A thin wrapper around create-react-app that adds extra functionality needed on most iot projects
+A thin wrapper around create-react-app that brings in all the dependencies needed to build a Watson IoT application
+
+## To use
+
+Kick off the cli by typing the following into your terminal
+
+```
+npx create-iot-react-app <app-name>
+```
+
+When this process is completed cd into the project folder you just created
+
+```
+cd <app-name>
+```
+
+you can either build components using storybook
+
+```
+yarn storybook
+```
+
+or start up your application
+
+```
+yarn start
+```
--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const colors = require('colors');
+
 const { exec } = require('child_process');
 const templates = require('./templates/templates.js');
 
 const appName = process.argv[2];
 const appDirectory = `${process.cwd()}/${appName}`;
 
+colors.setTheme({
+  silly: 'rainbow',
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red',
+  custom: ['cyan', 'bold'],
+});
+
 const createReactApp = () => new Promise((resolve) => {
     if (appName) {
       exec(`npx create-react-app ${appName}`, (error, stdout, stderr) => {
         if (error) {
-          console.error(`exec error: ${error}`);
+          console.error(`exec error: ${error}`.error);
           resolve(false);
         }
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-        console.log('Created react app, now installing storybook.');
+        console.log(`stdout: ${stdout}`.data);
+        console.log(`stderr: ${stderr}`.data);
+        console.log('Created react app, now installing storybook.'.custom);
         resolve(true);
       });
     } else {
-      console.log('\nNo app name was provided.');
-      console.log('\nProvide an app name in the following format: ');
-      console.log('\ncreate-iot-react-app ', 'app-name\n');
+      console.log('\nNo app name was provided.'.help);
+      console.log('\nProvide an app name in the following format: '.help);
+      console.log('\ncreate-iot-react-app ', 'app-name\n'.help);
       resolve(false);
     }
   });
@@ -32,12 +48,14 @@ const cdAndStorybook = () => new Promise((resolve, reject) => {
       `cd ${appName} && npx -p @storybook/cli sb init`,
       (error, stdout, stderr) => {
         if (error) {
-          console.error(`exec error: ${error}`);
+          console.error(`exec error: ${error}`.error);
           reject();
         }
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-        console.log('Storybook added, now installing supporting packages.');
+        console.log(`stdout: ${stdout}`.data);
+        console.log(`stderr: ${stderr}`.data);
+        console.log(
+          'Storybook added, now installing supporting packages.'.custom,
+        );
         resolve();
       },
     );
@@ -45,12 +63,13 @@ const cdAndStorybook = () => new Promise((resolve, reject) => {
 
 const installPackages = () => new Promise((resolve) => {
     console.log(
-      '\nInstalling node-sass, @reach/router, react-intl, carbon-components, carbon-components-react, carbon-icons, @carbon/icons,  @carbon/icons-react, @carbon/colors, @carbon/grid, @carbon/layout, @carbon/themes, carbon-addons-iot-react@beta, wiot-theme, classnames\n',
+      '\nInstalling node-sass, @reach/router, react-intl, carbon-components, carbon-components-react, carbon-icons, @carbon/icons,  @carbon/icons-react, @carbon/colors, @carbon/grid, @carbon/layout, @carbon/themes, carbon-addons-iot-react@beta, wiot-theme, classnames\n'
+        .help,
     );
     exec(
       `yarn --cwd ${appDirectory} add node-sass @reach/router react-intl carbon-components carbon-components-react carbon-icons @carbon/icons @carbon/icons-react @carbon/colors @carbon/grid @carbon/layout @carbon/themes carbon-addons-iot-react@beta classnames`,
       () => {
-        console.log('\nFinished installing packages\n');
+        console.log('\nFinished installing packages\n'.custom);
         resolve();
       },
     );
@@ -78,18 +97,24 @@ const updateTemplates = () => new Promise((resolve) => {
   });
 
 const run = async () => {
-  console.log('Starting install of WIoT React app, this may take a minute.');
+  console.log(
+    'Starting install of WIoT React app, this may take a minute.'.custom,
+  );
   const success = await createReactApp();
   if (!success) {
     console.log(
-      'Something went wrong while trying to create a new React app using create-react-app',
+      'Something went wrong while trying to create a new React app using create-react-app'
+        .error,
     );
     return false;
   }
   await cdAndStorybook();
   await installPackages();
   await updateTemplates();
-  console.log('All done');
+  console.log(
+    'All done. To get started `cd` into your project folder and type `yarn start`'
+      .help,
+  );
 };
 
 run();
